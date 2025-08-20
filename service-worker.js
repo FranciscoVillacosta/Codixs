8b113cb8f7b33479de5986b52bc84f746834ec05
Francisco Villa <franciscovillaacosta9@gmail.com>
2025-08-19 21:19:51 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "menu-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "style.css",
+  "script.js"
+];
+
+// Instalar y guardar en caché
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Servir desde caché sin internet
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
